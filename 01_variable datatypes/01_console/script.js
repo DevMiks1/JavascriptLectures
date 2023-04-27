a8f79e5976b71779cbca46dcfdb0d3dea466bf31
DevMiks1 <MikeAlfredVargas1@gmail.com>
2023-04-27 15:17:21 +0800

    Initial Commit

---
+++ 01_variable datatypes/01_console/script.js
@@ -0,0 +1,27 @@
+// alert ("Hello from JS file");
+
+// log the number
+console.log(100);
+
+// log the string
+console.log("Hello World");
+
+// log multiple values
+console.log(20, "Hello" ,true);
+
+// log a variable
+const x = 100;
+console.log(x);
+
+// console error and warning
+console.error("Alert");
+console.warn("Warning");
+
+console.table({name: "Mikee", email: "Mike@gmail.com"});
+
+// Group Console commands
+console.group("Simple");
+console.log(x);
+console.error("Alert");
+console.warn("Warning");
+console.groupEnd();
