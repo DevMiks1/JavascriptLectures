2b39aa6829d869108c2b426a3d2c7097d4a7e22d
DevMiks1 <MikeAlfredVargas1@gmail.com>
2023-04-27 15:37:10 +0800

    Initial Commit

---
+++ 01_variable datatypes/02_comments_shortcut/script.js
@@ -0,0 +1,9 @@
+// single line of code comment
+
+// the shortcut key is :  ctrl + / 
+
+// multi line comment shortcut key :  shift + alt + a 
+
+/* console.log(100);
+console.log("Hello World");
+console.log(20, "Hello", true); */
