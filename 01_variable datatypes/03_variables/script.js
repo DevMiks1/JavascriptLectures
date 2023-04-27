a0ee0d976bea80b335157adebf86a9fb59bec48f
DevMiks1 <MikeAlfredVargas1@gmail.com>
2023-04-27 16:51:26 +0800

    Updated

---
+++ 01_variable datatypes/03_variables/script.js
@@ -0,0 +1,31 @@
+// ways of declaring variable
+// var, let and const 
+
+
+// naming conventions
+// only letters, numbers, underscore and dollar signs
+// can't start with a number
+
+// multi word formatting
+
+
+const arr = [1,2,3,4];
+console.log(arr);
+// we can manipulate arrays and objects using const 
+arr.push(5)
+console.log(arr);
+
+const person = {
+    name : "Mike"
+};
+person.name = "Mikee";
+person.name = "Mike@gmail.com";
+console.log(person);
+
+// declare multiple values at once 
+let a,b,c; 
+
+const d=10,e=20,f=30
+console.log(d);
+console.log(e);
+console.log(f);
